63a12cc18d501c1b20f336fadc490fa76354f96c
Thomas Alexander Woolff (t9r) <143071425+ThomasWoolff@users.noreply.github.com>
2023-10-20 04:56:41 +0200

    nav redesign

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,5 @@
 import css from './header.module.css'
 import Logo from '../../assets/images/Logo.svg?react'
-import Person from '../../assets/images/Person.svg?react'
 import Burger from '../../assets/images/Burger.svg?react'
 
 const Header = () => {
@@ -8,10 +7,6 @@ const Header = () => {
 		<header>
 			<a href="/"><Logo /></a>
 			<nav className={css.rightFlex}>
-				<div>
-					<p>Log ud</p>
-					<Person />
-				</div>
 				<div>
 					<p>Hvad kan vi hjælpe med?</p>
 					<Burger />
