9069ed1395e413bc815305ba843a7b150cb86219
Thomas Alexander Woolff (t9r) <143071425+ThomasWoolff@users.noreply.github.com>
2023-10-20 14:52:24 +0200

    und so weiter

--- src/pages/category/categoryclaims.tsx
+++ src/pages/category/categoryclaims.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { IData } from '../../components/App'
 import Arrow from '../../assets/images/Arrow.svg?react'
 import Glyph from '../../assets/images/Glyph.svg?react'
@@ -10,61 +11,59 @@ interface ClaimsCatProps {
 
 const ClaimsCat = ({ filteredCatData }: ClaimsCatProps) => {
     const [subItems, setSubItems] = useState<IData[]>([])
-    
-    const filterSubcatByCat = (subcat: IData[], cat: string): IData[] => { return subcat.filter((item) => item.cat === cat) }
-    
-    const filterSubcatByGroup = (subcat: IData[], group: string, cat: string): IData[] => {
-        return subcat.filter((item) => item.group === group && item.cat !== cat)
-    }
+    const navigate = useNavigate()
 
     const handleSubItemClick = (group: string, cat: string) => {
         const subItems = filteredCatData.flatMap((item) =>
-            filterSubcatByGroup(item.subcat || [], group, cat)
+            item.subcat?.filter((subItem) => subItem.group === group && subItem.cat !== cat) || []
         )
         setSubItems(subItems)
     }
 
-    console.log(subItems)
+    const handleDeepClick = (subItem: IData) => {
+        const query = { deepestObject: JSON.stringify(subItem) }
+        navigate({
+            pathname: '/form',
+            search: '?' + new URLSearchParams(query).toString(),
+        })
+    }
+
     return (
         <>
-            {filteredCatData.map((item) => {
-                return (
-                    <section key={item.id} className={css.category}>
-                        <button className={`${css.claim} ${css.active}`} onClick={() => { window.location.href = '/' }}>
-                            <div className={css.claimInfo}>
-                                <h2>{subItems.length === 0 ? item.title : subItems[0].parent}</h2>
-                            </div>
-                            <Arrow />
-                        </button>
-                        {subItems.length === 0 && item.subcat &&
-                            filterSubcatByCat(item.subcat, item.cat).map((subItem) => {
-                                return (
-                                    <button className={`${css.claim} ${css.subcat}`} key={subItem.id} onClick={() => handleSubItemClick(subItem.group, subItem.cat)}>
-                                        <div className={css.claimInfo}>
-                                            <h2>{subItem.title}</h2>
-                                        </div>
-                                        <Glyph />
-                                    </button>
-                                )
-                            })}
-                        {subItems.length > 0 && (
-                            <div className={css.subItems}>
-                                {subItems.map((subItem) => (
-                                    <button className={`${css.claim} ${css.subcat}`} key={subItem.id} onClick={() => handleSubItemClick(subItem.group, subItem.cat)}>
-                                        <div className={css.claimInfo}>
-                                            <h2>{subItem.title}</h2>
-                                        </div>
-                                        <Glyph />
-                                    </button>
-                                ))}
-                            </div>
-                        )}
-                        <div className={css.back}>
-                            <a href='/'>Tilbage til forside</a>
+            {filteredCatData.map((item) => (
+                <section key={item.id} className={css.category}>
+                    <button className={`${css.claim} ${subItems.length === 0 ? css.active : css.active}`} onClick={() => setSubItems([])}>
+                        <div className={css.claimInfo}>
+                            <h2>{subItems.length === 0 ? item.title : subItems[0].parent}</h2>
                         </div>
-                    </section>
-                )
-            })}
+                        <Arrow />
+                    </button>
+                    {subItems.length === 0 &&
+                        item.subcat?.filter((subItem) => subItem.cat === item.cat).map((subItem) => (
+                            <button className={`${css.claim} ${css.subcat}`} key={subItem.id} onClick={() => handleSubItemClick(subItem.group, subItem.cat)}>
+                                <div className={css.claimInfo}>
+                                    <h2>{subItem.title}</h2>
+                                </div>
+                                <Glyph />
+                            </button>
+                        ))}
+                    {subItems.length > 0 && (
+                        <div className={css.subItems}>
+                            {subItems.map((subItem) => (
+                                <button className={`${css.claim} ${css.subcat}`} key={subItem.id} onClick={() => (!subItem.form ? handleSubItemClick(subItem.group, subItem.cat) : handleDeepClick(subItem))}>
+                                    <div className={css.claimInfo}>
+                                        <h2>{subItem.title}</h2>
+                                    </div>
+                                    <Glyph />
+                                </button>
+                            ))}
+                        </div>
+                    )}
+                    <div className={css.back}>
+                        <a href='/'>Tilbage til forside</a>
+                    </div>
+                </section>
+            ))}
         </>
     )
 }
