934874d3e216c79f2489da7a7079d5dd79d3d991
Thomas Alexander Woolff (t9r) <143071425+ThomasWoolff@users.noreply.github.com>
2023-10-24 10:43:36 +0200

    ...

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,21 +1,18 @@
-import { useContext, useState, useEffect, useRef } from 'react'
+import { useContext, useState } from 'react'
 import { DataContext, IAppState } from '../App'
 import css from './input.module.css'
 
-interface InputProps {
-	onClick?: () => void
-  }
+interface InputProps { onClick?: () => void }
 
 const Input = ({ onClick }: InputProps) => {
 	const { setQuery } = useContext<IAppState>(DataContext)
 	const [placeholder, setPlaceholder] = useState('Søg på skade')
-	const inputRef = useRef<HTMLInputElement>(null)
 	const handleFocus = () => {setPlaceholder('')}
 	const handleBlur = () => {setPlaceholder('Søg på skade')}
 
 	return (
 		<div className={css.inputField}>
-			<input type='text' onChange={(e) => setQuery(e.target.value)} placeholder={placeholder} onFocus={handleFocus} onBlur={handleBlur} onClick={onClick} ref={inputRef} />
+			<input type='text' onChange={(e) => setQuery(e.target.value)} placeholder={placeholder} onFocus={handleFocus} onBlur={handleBlur} onClick={onClick} />
 		</div>
 	)
 }
