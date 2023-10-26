2aaddc9cd553e80d8cbdd1818af9c7ef07ff2053
Thomas Alexander Woolff (t9r) <143071425+ThomasWoolff@users.noreply.github.com>
2023-10-26 07:54:37 +0200

    ,,,

--- src/components/claims/claim.tsx
+++ src/components/claims/claim.tsx
@@ -3,7 +3,6 @@ import Arrow from '../../assets/images/Arrow.svg?react'
 import css from './claims.module.css'
 
 const Claim = ({ title, description, className, cat, filteredWords, onClick}: IData) => {
-	console.log(filteredWords)
 	return (
 		<button className={className} onClick={onClick}>
 			<div className={css.claimInfo}>
